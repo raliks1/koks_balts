9f47d7eaa7551ae8abe2b3e27eff3ad4ef5eaea8
Ralfs Aliks Romāns <raliks@edu.riga.lv>
2024-10-03 12:43:37 +0300

    Update helloo.py

--- helloo.py
+++ helloo.py
@@ -2,18 +2,23 @@ print("sveika pasaule")
 print((7+6*6))
 
 print("Sveiks, Andris!")
-input("Kā tevi sauc? ")
+vards = input("Kā tevi sauc? ")
 
 x = 2 + 3
 x= x+1
 print(x)
 
-a = 12
-b = 7
-print(a+b)
+# a = 12
+# b = 7
+# print(a+b)
 
-a = 12
-b = 7
-print(a+b)
-print(b)
-print("b")
+# a = 12
+# b = 7
+# print(a+b)
+# print(b)
+# print("b")
+# sasveicinos
+
+vecums = input("cik tev gadu? ")
+print("sveiks, " + vards)
+print("tev ir ", vecums, "gadi")
