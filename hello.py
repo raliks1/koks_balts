6705612288eb9e7da043989331ca0d5a4a3265b9
Ralfs Aliks Romāns <raliks@edu.riga.lv>
2024-10-10 11:12:23 +0300

    darbs

---
+++ hello.py
@@ -0,0 +1,11 @@
+# prasa lietotājam vārdu
+# name = input("Kā tevi sauc? ")
+
+# saka Sveiks lietotājiem
+# print("sveiks, ")
+# print(name)
+
+print("Sveiki, \"labie skolēni\"")
+# print(*objects, sep=' ', ends'\n', file=sys.stdout, flush=False)
+print("Ralfs", "Aliks-Romāns", sep='?????')
+print("Sveiks, ", end="nia")
