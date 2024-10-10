d178f47fae18393dbbc58985323ec93cddfecb68
Ralfs Aliks Romāns <raliks@edu.riga.lv>
2024-10-10 11:27:58 +0300

    darbs

--- hello.py
+++ hello.py
@@ -5,7 +5,14 @@
 # print("sveiks, ")
 # print(name)
 
-print("Sveiki, \"labie skolēni\"")
+# print("Sveiki, \"labie skolēni\"")
 # print(*objects, sep=' ', ends'\n', file=sys.stdout, flush=False)
-print("Ralfs", "Aliks-Romāns", sep='?????')
-print("Sveiks, ", end="nia")
+# print("Ralfs", "Aliks-Romāns", sep='?????')
+# print("Sveiks, ", end="")
+name = input("Kā tevi sauc? ")
+
+name = name.strip().title()
+
+
+print("Sveiks,", name)
+print(f"Sveiks, {name}")
