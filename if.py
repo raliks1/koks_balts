95ae0e67056d01cdb1c64f632113b7491c6d32b2
Ralfs Aliks Romāns <raliks@edu.riga.lv>
2024-10-31 12:48:31 +0200

    Update if.py

--- if.py
+++ if.py
@@ -9,15 +9,31 @@ print("\n") # ieliek tuksu rindinu pirms izdrukas
     #print("x nav vienāds y")
 #else:                                 # or    elif x == y:
     #print("x ir vienāds ar y")
-punkti = int(input("punkti:"))
+#punkti = int(input("punkti:"))
 
-if punkti >= 90 and punkti <= 100:
-    print("Vērtējums: 10")
-elif punkti >=80 and punkti < 90:
-    print("Vērtējums: 9")
-elif punkti >=70 and punkti < 80:
-    print("Vērtējums: 8")
-elif punkti >=60 and punkti < 70:
-    print("Vērtējums: 7")
+#if punkti >= 90 and punkti <= 100:
+ #   print("Vērtējums: 10")
+#elif punkti >=80 and punkti < 90:
+ #   print("Vērtējums: 9")
+#elif punkti >=70 and punkti < 80:
+ #   print("Vērtējums: 8")
+#elif punkti >=60 and punkti < 70:
+ #   print("Vērtējums: 7")
+#else:
+ #   print("Vērtējums: n/v")
+
+import math
+
+a = int(input("Kāda ir a vērtība? "))
+b = int(input("Kāda ir b vērtība? "))
+c = int(input("Kāda ir c vērtība? "))
+d=b**2-4*a*c
+if d>0:
+    x1=(-b+d)/2*a
+    x2=(-b-d)/2*a
+elif d,0
+elif d==0:
+    x=b/2*a
+    print(f'x=(x)')
 else:
-    print("Vērtējums: n/v")
+    print("nav definēts")
