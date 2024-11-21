f0dad1795a178740826cd10519fbab8f3424336a
Ralfs Aliks Romāns <raliks@edu.riga.lv>
2024-11-21 11:11:17 +0200

    jauna tema
    tema

---
+++ jauna tema.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls') #attira terminala logu pasa sakuma
+print("\n") # ieliek tuksu rindinu pirms izdrukas
+
+i = 3
+while i != 0:
+    print("Es esmu gudrs")
+
+
+
+print("Es esmu gudrs")
+print("Es esmu gudrs")
+print("Es esmu gudrs")
