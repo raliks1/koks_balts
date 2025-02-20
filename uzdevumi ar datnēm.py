e4edd54fd681145a388c9e83fce515a09dde3168
Ralfs Aliks Romāns <raliks@edu.riga.lv>
2025-02-20 12:09:27 +0200

    blabla
    blabla

---
+++ uzdevumi ar datnēm.py
@@ -0,0 +1,65 @@
+import os
+os.system('cls')
+print("\n")
+
+# 1. Vienkārša teksta rakstīšana un lasīšana no faila 
+
+# file = open("Teksts.txt", "w") 
+
+# vārds = input("Ievadi tekstu: ") 
+# file.write(vārds) 
+# file.write("\n") 
+    
+# file.close() 
+
+# print("Teksts ir ierakstīts failā") 
+
+# 2. Rindu skaitīšana failā 
+
+with open('data.txt', 'r') as file:
+    kontents = file.read()
+    line_count = kontents.count('\n')
+    print("Kopā līnijas ir:", line_count)
+    
+# 3. Vārda sastopamības skaitīšana failā
+
+# file = open("Data.txt", "w") 
+
+# vārds = input("Ievadi tekstu: ") 
+# file.write(vārds) 
+# file.write("\n") 
+    
+# file.close()
+
+# text = open("Data.txt", "r") 
+# d = dict() 
+# for line in text: 
+# 	line = line.strip() 
+# 	line = line.lower() 
+# 	words = line.split(" ") 	
+# 	for word in words: 
+# 		if word in d: 
+# 			d[word] = d[word] + 1
+# 		else: 
+# 			d[word] = 1
+# for key in list(d.keys()): 
+# 	print(key, ":", d[key]) 
+
+# 4. Datu pievienošana failam 
+
+# 5. Apvienot divus failus, pārmaiņus ierakstot rindas
+
+
+
+
+
+
+# 6. Vārdu atkārtošanās skaita noteikšana failā
+
+# 7. Studentu atzīmju analīze no faila
+
+# 8. Liela faila apstrāde un analīze (Augstāks līmenis)
+
+# 9. Faila filtrēšana un jauna faila izveide (Augstāks līmenis)
+
+# 10. Ezeru saraksta izveide RTF failā
