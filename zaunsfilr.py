a3a9e7ec433542dc4e0f5c672698ab20b5fb6d3c
Ralfs Aliks Romāns <raliks@edu.riga.lv>
2025-02-27 12:03:46 +0200

    .
    .

---
+++ zaunsfilr.py
@@ -0,0 +1,27 @@
+import os
+os.system('cls')
+print("\n")
+
+import pandas
+from datetime import datetime
+import ctypes 
+
+df = pandas.read_csv("saraksts.csv", sep=';', dtype=str)
+# print(df.to_string)
+
+today = datetime.today().strftime("%d.%m")
+
+df["Dzimšanas datums"] = df["Dzimšanas datums"].str[:5]
+
+bd_skoleni = df[df["Dzimšanas datums"] == today]
+
+print(bd_skoleni)
+
+if not bd_skoleni.empty:
+    teksts = "Sveicam dzimšanas dienā!!!\n\n"
+    for _, row in bd_skoleni.iterrows():
+        teksts += f"{row['Vārds'].upper()}\n{row['Klase'].upper()}\n{row['Deklarētā adrese'].upper()}\n\n"
+else:
+    teksts = "Šodien nav nevienam dz.diena!!!!"
+
+ctypes.windll.user32.MessageBoxW(0,teksts.strip(), "svinam", 1)
